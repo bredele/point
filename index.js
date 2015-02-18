794c827a97035e04288e3f9b63bce1a30ec28e1c
bredele <olivier.wietrich@gmail.com>
2015-02-17 19:08:16 -0700

    initialize component

---
+++ index.js
@@ -0,0 +1,24 @@
+
+/**
+ * Expose 'Point'
+ */
+
+module.exports = Point;
+
+
+/**
+ * Point constructor.
+ * @api public
+ */
+
+function Point() {
+  
+}
+
+Point.prototype.context = function() {
+	
+};
+
+Point.prototype.draw = function() {
+	
+};
